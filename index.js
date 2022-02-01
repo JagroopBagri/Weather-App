5dd6cff37f968232e0875e199591ce63be87b730
Jagroop <jagroopbagri5@gmail.com>
2022-02-01 00:04:38 -0800

    Updated functionality

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 // Main function
 function startApp(){
+    // DOM variables
+
     // search bar 
     const searchBar = document.querySelector('#location');
+    // search button
+    const searchButton = document.querySelector('.searchButton');
     // todays description div
     const todaysDescriptionDiv = document.querySelector('.brief-description');
     // todays temp div
@@ -24,10 +28,33 @@ function startApp(){
     const day3MinMaxDiv = document.querySelector(".day3-min-max");
     // day 4 min-max div
     const day4MinMaxDiv = document.querySelector(".day4-min-max");
+    // day 2 name div
+    const day2NameDiv = document.querySelector(".day2-date");
+    // day 3 name div
+    const day3NameDiv = document.querySelector(".day3-date");
+    // day 4 name div
+    const day4NameDiv = document.querySelector(".day4-date");
+    // todays weather icon
+    const mainWeatherIcon = document.querySelector(".todays-weather-icon");
+    // tomorrows weather icon
+    const tomWeatherIcon = document.querySelector(".day1image");
+    // day2 weather icon
+    const day2WeatherIcon = document.querySelector(".day2image");
+    // day3 weather icon
+    const day3WeatherIcon = document.querySelector(".day3image");
+    // day4 weather icon
+    const day4WeatherIcon = document.querySelector(".day4image");
+
+
     // function that retrieves weather data and appends it to the DOM
     const getAndAppendWeatherInfo = async function(){
+   
         // Attempt to fetch data 
         try{
+            // check to see if searchbar has a value
+            if(searchBar.value === '' || searchBar.value === undefined){
+                searchBar.value = "San Francisco";
+            }
             // retrieve data from API
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value}&units=imperial&appid=dbb4032d6b4dd101b94f20c0de6f3e12`, {mode: "cors"});
             // convert data to JSON format
@@ -39,15 +66,50 @@ function startApp(){
             const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&units=imperial&appid=dbb4032d6b4dd101b94f20c0de6f3e12`, {mode: "cors"});
             // convert forecast response to JSON
             const forecastData = await forecastResponse.json();
+
             // probability of precipitation
             const probablityOfRain = forecastData.daily[0].pop;
             // append probability of precipitation
             chanceOfRainDiv.textContent = Math.round(probablityOfRain * 100) + '%';
             console.log(weatherData);
-            // weather description
-            const weatherDescription = weatherData.weather[0].description;
+
+
+            // main weather description 
+            const mainWeatherDescription = weatherData.weather[0].main;
+            const mainDetailedDescriptionID = weatherData.weather[0].id;
+            // function that appends weather description
+            const appendCurrentWeatherDescription = function(){
+                let description = "it's clear.";
+                if(mainWeatherDescription === "Clear"){
+                    description = "the sky is clear.";
+                }
+                else if (mainDetailedDescriptionID  === 801 || mainDetailedDescriptionID  === 802){
+                    description = "the sky is slightly cloudy.";
+                }
+                else if (mainDetailedDescriptionID  === 803){
+                    description = "the sky is cloudy.";
+                }
+                else if (mainDetailedDescriptionID  === 804){
+                    description = "the sky is overcast.";
+                }
+                else if (mainWeatherDescription === "Rain"){
+                    description = "it's raining.";
+                }
+                else if (mainWeatherDescription === "Drizzle"){
+                    description = "there's light rain.";
+                }
+                else if (mainWeatherDescription === "Thunderstorm"){
+                    description = "there's a thunderstorm.";
+                }
+                else if (mainWeatherDescription === "Snow"){
+                    description = "it's snowing.";
+                }
             // append weather description
-            todaysDescriptionDiv.textContent = weatherDescription;
+            todaysDescriptionDiv.textContent = `Currently in ${searchBar.value}, ${description}`;
+            };
+            // invoke appendCurrentWeatherDesciption function
+            appendCurrentWeatherDescription();
+
             // current temp
             const weatherTemp = weatherData.main.temp;
             // append current temp (round the temp and add degree symbol)
@@ -85,11 +147,233 @@ function startApp(){
             const day4MinMax = Math.round(forecastData.daily[4].temp.min) + "°" + " / " + Math.round(forecastData.daily[4].temp.max) + "°";
             // append day4 min-max
             day4MinMaxDiv.textContent = day4MinMax;
+
+            // function to get current day name
+            let getDay = function(num){
+                let currentDate = new Date();
+                let currentDay = currentDate.getDate();
+                let x = currentDate.setDate(currentDay + num);
+                let y = new Date(x).getDay();
+                let daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+                return daysOfTheWeek[y];
+            }
+            // append forecast day names
+            day2NameDiv.textContent = getDay(2);
+            day3NameDiv.textContent = getDay(3);
+            day4NameDiv.textContent = getDay(4);
+            
+            // retrieve time information in order to display correct icons
+            // get sunrise hour
+            let sunriseTime = (new Date(forecastData.current.sunrise * 1000)).toLocaleString("en-US", {timeZone: forecastData.timezone, hour: 'numeric', hour12: false});
+            // get sunset hour
+            let sunsetTime = (new Date(forecastData.current.sunset * 1000)).toLocaleString("en-US", {timeZone: forecastData.timezone, hour: 'numeric', hour12: false});
+            // current time in location timezone
+            let currentLocationTime = new Date().toLocaleString("en-US", {timeZone: forecastData.timezone, hour: 'numeric', hour12: false});
+            // variable that displays whether it is night time or day time
+            let dayTime = true;
+            // function that updates whether it is day time or night time variable
+            const checkDayOrNight = function(){
+                if(currentLocationTime > sunriseTime && currentLocationTime < sunsetTime){
+                    dayTime = true;
+                }
+                else{
+                    dayTime = false;
+                }
+                return dayTime;
+            };
+            // invoke check day or night time function
+            checkDayOrNight();
+
+            // retrieve current weather condition id number
+            const currentWeatherConditionID = forecastData.current.weather[0].id;
+            // function that appends correct weather icon to current day
+            const appendMainIcon = function(){
+                if (currentWeatherConditionID > 199 && currentWeatherConditionID < 234){
+                    mainWeatherIcon.src = "images/storm.png";
+                }
+                else if (currentWeatherConditionID > 299 && currentWeatherConditionID < 322){
+                    mainWeatherIcon.src = "images/rain.png";
+                }
+                else if (currentWeatherConditionID === 502 || currentWeatherConditionID === 503 || currentWeatherConditionID === 504){
+                    mainWeatherIcon.src = "images/downpour.png";
+                }
+                else if (currentWeatherConditionID > 499 && currentWeatherConditionID < 532){
+                    mainWeatherIcon.src = "images/rain.png";
+                }
+                else if (currentWeatherConditionID > 599 && currentWeatherConditionID < 623){
+                    mainWeatherIcon.src = "images/snow.png";
+                }
+                else if (currentWeatherConditionID  === 804){
+                    mainWeatherIcon.src = "images/overcast.png";
+                }
+                else if (currentWeatherConditionID > 800){
+                    if(dayTime){
+                        mainWeatherIcon.src = "images/day-cloudy.png";
+                    }
+                    else{
+                        mainWeatherIcon.src = "images/night-cloudy.png";
+                        }
+                }
+                else{
+                    if(dayTime){
+                        mainWeatherIcon.src = "images/sunny.png";
+                        }
+                        else{
+                        mainWeatherIcon.src = "images/night.png";
+                        }
+                }
+            };
+            // invoke appendMainIcon function
+            appendMainIcon();
+
+            // retrieve tomorrows weather condition id number
+            const tomWeatherConditionID = forecastData.daily[1].weather[0].id;
+            // function that appends correct weather icon to next day
+            const appendTomIcon = function(){
+                if (tomWeatherConditionID > 199 && tomWeatherConditionID < 234){
+                    tomWeatherIcon.src = "images/storm.png";
+                }
+                else if (tomWeatherConditionID > 299 && tomWeatherConditionID < 322){
+                    tomWeatherIcon.src = "images/rain.png";
+                }
+                else if (tomWeatherConditionID === 502 || tomWeatherConditionID === 503 || tomWeatherConditionID === 504){
+                    tomWeatherIcon.src = "images/downpour.png";
+                }
+                else if (tomWeatherConditionID > 499 && tomWeatherConditionID < 532){
+                    tomWeatherIcon.src = "images/rain.png";
+                }
+                else if (tomWeatherConditionID > 599 && tomWeatherConditionID < 623){
+                    tomWeatherIcon.src = "images/snow.png";
+                }
+                else if (tomWeatherConditionID === 804){
+                    tomWeatherIcon.src = "images/overcast.png";
+                }
+                else if (tomWeatherConditionID > 800){
+                        tomWeatherIcon.src = "images/day-cloudy.png";
+                }
+                else{
+                        tomWeatherIcon.src = "images/sunny.png";
+                }
+            };
+            // invoke appendTomIcon function
+            appendTomIcon();
+
+            // retrieve day2 weather condition id number
+            const day2WeatherConditionID = forecastData.daily[2].weather[0].id;
+            // function that appends correct weather icon to next day
+            const appendDay2Icon = function(){
+                if (day2WeatherConditionID > 199 && day2WeatherConditionID < 234){
+                    day2WeatherIcon.src = "images/storm.png";
+                }
+                else if (day2WeatherConditionID > 299 && day2WeatherConditionID < 322){
+                    day2WeatherIcon.src = "images/rain.png";
+                }
+                else if (day2WeatherConditionID === 502 || day2WeatherConditionID === 503 || day2WeatherConditionID === 504){
+                    day2WeatherIcon.src = "images/downpour.png";
+                }
+                else if (day2WeatherConditionID > 499 && day2WeatherConditionID < 532){
+                    day2WeatherIcon.src = "images/rain.png";
+                }
+                else if (day2WeatherConditionID > 599 && day2WeatherConditionID < 623){
+                    day2WeatherIcon.src = "images/snow.png";
+                }
+                else if (day2WeatherConditionID === 804){
+                    day2WeatherIcon.src = "images/overcast.png";
+                }
+                else if (day2WeatherConditionID > 800){
+                    day2WeatherIcon.src = "images/day-cloudy.png";
+                }
+                else{
+                    day2WeatherIcon.src = "images/sunny.png";
+                }
+            };
+            // invoke appendDay2Icon function
+            appendDay2Icon();   
+
+            // retrieve day3 weather condition id number
+            const day3WeatherConditionID = forecastData.daily[3].weather[0].id;
+            // function that appends correct weather icon to next day
+            const appendDay3Icon = function(){
+                if (day3WeatherConditionID > 199 && day3WeatherConditionID < 234){
+                    day3WeatherIcon.src = "images/storm.png";
+                }
+                else if (day3WeatherConditionID > 299 && day3WeatherConditionID < 322){
+                    day3WeatherIcon.src = "images/rain.png";
+                }
+                else if (day3WeatherConditionID === 502 || day3WeatherConditionID === 503 || day3WeatherConditionID === 504){
+                    day3WeatherIcon.src = "images/downpour.png";
+                }
+                else if (day3WeatherConditionID > 499 && day3WeatherConditionID < 532){
+                    day3WeatherIcon.src = "images/rain.png";
+                }
+                else if (day3WeatherConditionID > 599 && day3WeatherConditionID < 623){
+                    day3WeatherIcon.src = "images/snow.png";
+                }
+                else if (day3WeatherConditionID === 804){
+                    day3WeatherIcon.src = "images/overcast.png";
+                }
+                else if (day3WeatherConditionID > 800){
+                    day3WeatherIcon.src = "images/day-cloudy.png";
+                }
+                else{
+                    day3WeatherIcon.src = "images/sunny.png";
+                }
+            };
+            // invoke appendDay3Icon function
+            appendDay3Icon();   
+
+            // retrieve day4 weather condition id number
+            const day4WeatherConditionID = forecastData.daily[4].weather[0].id;
+            // function that appends correct weather icon to next day
+            const appendDay4Icon = function(){
+                if (day4WeatherConditionID > 199 && day4WeatherConditionID < 234){
+                    day4WeatherIcon.src = "images/storm.png";
+                }
+                else if (day4WeatherConditionID > 299 && day4WeatherConditionID < 322){
+                    day4WeatherIcon.src = "images/rain.png";
+                }
+                else if (day4WeatherConditionID === 502 || day4WeatherConditionID === 503 || day4WeatherConditionID === 504){
+                    day4WeatherIcon.src = "images/downpour.png";
+                }
+                else if (day4WeatherConditionID > 499 && day4WeatherConditionID < 532){
+                    day4WeatherIcon.src = "images/rain.png";
+                }
+                else if (day4WeatherConditionID > 599 && day4WeatherConditionID < 623){
+                    day4WeatherIcon.src = "images/snow.png";
+                }
+                else if (day4WeatherConditionID === 804){
+                    day4WeatherIcon.src = "images/overcast.png";
+                }
+                else if (day4WeatherConditionID > 800){
+                    day4WeatherIcon.src = "images/day-cloudy.png";
+
+                }
+                else{
+                    day4WeatherIcon.src = "images/sunny.png";
+                }
+            };
+            // invoke appendDay4Icon function
+            appendDay4Icon();   
+
+            //clear searchbar
+            searchBar.value = '';
+
         // if any error occurs along the way then log the error
         } catch(error){
+            alert('Location not found. Search must be in the form of "City", "City, State" or "City, Country".');
             console.error(error);
         }
     };
     getAndAppendWeatherInfo();
+
+    // Add event listener to search button
+    searchButton.addEventListener('click', getAndAppendWeatherInfo);
+    // add event listener to enter key being pressed
+    searchBar.addEventListener('keypress', event => {
+        if (event.key === "Enter"){
+            event.preventDefault();
+            searchButton.click();
+        }
+    })
 }
 startApp();
